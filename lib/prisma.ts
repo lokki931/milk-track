15f53c842e67593b9fba00057c5005d6e53f6d88
lokki931 <taurusr2d2@gmail.com>
2025-05-14 13:42:58 +0300

    change prisma ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from "@/lib/generated/prisma";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
 };
 
 const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+  globalForPrisma.prisma ??
+  (new PrismaClient().$extends(withAccelerate()) as unknown as PrismaClient);
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
